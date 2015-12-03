610d86c434a80e2e2942caa4493f12a36ce47f2e
Matt Smith <mjsmith@uchicago.edu>
2015-12-03 16:25:59 -0600

    working on bib

--- Code/makebib.py
+++ Code/makebib.py
@@ -1,30 +1,39 @@
+import re
 
 #### author parsing function
 def parseauth(author):
     lastname, firstname = [xx.strip() for xx in author.split(',')]
-    initials = firstname.split(' ').strip(' .')
+    initials = ''.join([xx[0] + '.' for xx in firstname.split(' ')])
+    ## sanitization (latex -> html)
+    lastname = lastname.replace("\\'a", '&aacute;')
     cc = 'author'
     if ((lastname == 'Smith' or lastname == 'Michalska-Smith') and firstname == 'Matthew J.'):
         cc = cc + ' pi'
-    return('<span class="' + cc + '">' + firstname.split(' ')[0][0] + '.' + firstname.split(' ')[1] + ' ' + lastname + '</span>')
+    return('<span class="' + cc + '">' + initials + ' ' + lastname + '</span>')
 
 #### read in the bibfile
 with open('../Documents/MJSbib.bib', 'r') as bibfile:
     bibtext = bibfile.read()
 
 #### parse the information
-## split by entry and store each as a dictionary element
-bibarray = bibtext.split('@')
-bibdict = {}
-## get a list of all the years so numbers can be assigned
-allyears = [xx[0] for xx in [re.findall(r'ear\s?=\s?{(\d\d\d\d)}', bibitem) for bibitem in bibarray] if xx]
+## split by entry and store each as a html string
+biblist = bibtext.split('@')
 
-## the stored element will be an html string
-for bibitem in bibarray:
+## sort array by year so numbers can be assigned
+biblist.sort(key=lambda xx: re.findall(r'ear\s?=\s?{(\d\d\d\d)}', xx), reverse=True)
+
+htmllist = []
+nn = 1
+for bibitem in biblist:
+    if not re.findall(r'ear\s?=\s?{(\d\d\d\d)}', bibitem):
+        continue
     bibinfo = bibitem.split('\n')
     ## extract the parts of the citation
     bibID = bibinfo[0].split('{')[1].strip(',')
     tmp = [xx.split('=') for xx in bibinfo[1:len(bibinfo)]]
+    ## initialize the links since they are optional
+    prelink = ''
+    jrnlink = ''
     for info in tmp:
         infotype = info[0].strip().lower()
         if infotype == 'title':
@@ -35,35 +44,38 @@ for bibitem in bibarray:
             year = info[1].strip(' {},')
         if infotype == 'journal':
             journal = info[1].strip(' {},')
-#        if infotype == '':
-#            prelink =
         if infotype == 'doi':
+            prelink = info[1].strip(' {},')
+        if infotype == 'url':
             jrnlink = info[1].strip(' {},')
     #### create the html bibentries
     bibhtml = ['<div class="row publication">',
                '  <div class="col-xs-1"></div>',
-               '  <div class="col-xs-8"><a name="$bibID"></a>',
-               '    <span class="pubnumber">' + pubnumber +'</span>',
-               '    <span class="pubtitle">$title</span><br>',
+               '  <div class="col-xs-8"><a name="'+ bibID + '"></a>',
+               '    <span class="pubnumber">' + str(nn) +'</span>',
+               '    <span class="pubtitle">' + title + '</span><br>',
                ## the author list needs to be parsed and my name accented
-               '    ' + ''.join([parseauth(auth)  for auth in authlist]),
-               '    <span class="pubyear">$year</span>',
+               '    ' + ', '.join([parseauth(auth)  for auth in authlist]) + ';',
+               '    <span class="pubyear">' + year + '</span>',
                '  </div>',
-               '  <div class="col-xs-2 col-xs-offset-1 text-right biblinks">',
-               '    <a href="$preprintlink">',
-               '      <span class="label label-info">Citable preprint</span>
-               '    </a><br>',
-               '    <a href="/pdfs/$bibID">',
-               '      <span class="label label-success">Full text PDF</span>',
-               '    </a><br>',
-               '    <a href="$journallink" title="$journal">',
-               '      <span class="label label-warning">Journal version</span>',
-               '    </a>',
-               '  </div>',
-               '</div>']
-    bibdict[pubnumber] = '\n'.join(bibhtml)
+               '  <div class="col-xs-2 col-xs-offset-1 text-right biblinks">']
+    if prelink:
+        bibhtml = bibhtml + ['    <a href="' + prelink + '">',
+                             '      <span class="label label-info">Citable preprint</span>',
+                             '    </a><br>']
+    bibhtml = bibhtml + ['    <a href="/pdfs/' + bibID + '">',
+                         '      <span class="label label-success">Full text PDF</span>',
+                         '    </a><br>']
+    if jrnlink:
+        bibhtml = bibhtml + ['    <a href="' + jrnlink + '" title="' + journal + '">',
+                             '      <span class="label label-warning">Journal version</span>',
+                             '    </a>']
+    bibhtml = bibhtml + ['  </div>',
+                         '</div>']
+    htmllist.append('\n'.join(bibhtml))
+    nn = nn + 1
 
 #### write to file
 with open('../bib.html', 'w') as htmlfile:
-    for 
+    htmlfile.write('\n\n'.join(htmllist))
     
