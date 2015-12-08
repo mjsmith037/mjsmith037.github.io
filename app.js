edd93bb2f7e96b709bfebe60fa6361304b851557
Matt Smith <mjsmith@uchicago.edu>
2015-12-08 15:11:33 -0600

    tried out jquery revealing of project info

--- app.js
+++ app.js
@@ -9,11 +9,19 @@ $(document).ready(function(){
         var id = $(e.target).attr("href").substr(1);
         window.location.hash = id;
     });
-
     // on load of the page: switch to the currently selected tab
     var hash = window.location.hash;
     $('#myTab a[href="' + hash + '"]').tab('show');
 
+    // import the bibliography
     $("#importBib").load("bib.html"); 
+    
+    // when a project is selected, center it and remove the others
+    // add a back button for return
+//    $(".projects .thumbnail").click(function(){
+//        $(this).parent().siblings().children().toggle();
+//        $(this).parent().toggleClass("col-xs-4").toggleClass("col-xs-12");
+//    });
+
 });
 
