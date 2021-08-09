156b7e620841bffa1c74b8d301fb15bad03c38c9
Matthew J. Michalska-Smith <mjsmith037@gmail.com>
2021-08-09 11:01:15 -0400

    added comment to makebib.py

--- Code/makebib.py
+++ Code/makebib.py
@@ -87,6 +87,7 @@ for bibitem in biblist:
                                  '      <button type="button" class="btn btn-preprint">Citable Preprint</button>',
                                  '    </a></p>']
         else:
+            # these papers were not written by me and I don't have access to editable pre-publication versions
             if bibID != "whirling" and bibID != "dispersalnetworks":
                 bibhtml = bibhtml + ['    <p><a target="_blank"  href="pdfs/' + bibID + '.pdf">',
                                      '      <button type="button" class="btn btn-mycopy">Full-Text PDF</button>',
